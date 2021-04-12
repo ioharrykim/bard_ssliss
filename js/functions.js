012e56c865781db980a6097098d52b48b8cdfbad
Hyunmin Kim <56422158+juventuslandia@users.noreply.github.com>
2021-04-12 11:39:25 +0900

    est
    tes

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+(function ($) {
+  $(".image img").click(function () {
+    window.open(
+      "https://ustudio.my.webex.com/ustudio.my-ko/j.php?MTID=m3cc52be67caca64b0a07a9cc3e25dbf9"
+    );
+  });
+})(jQuery);
---
+++ js/functions_0416.js
@@ -0,0 +1,5 @@
+(function ($) {
+  $(".btn-img").click(function () {
+    alert("강의실 입장은 4월 16일 부터 가능합니다.");
+  });
+})(jQuery);
